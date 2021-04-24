f8351a5b0daad57357b847230f5f72a78c88487c
Chih Yun <wuchihy@oreonstate.edu>
2021-04-23 17:26:20 -0700

    complete leap year program w/ error handling

--- leapYear_y.py
+++ leapYear_y.py
@@ -1,4 +1,10 @@
-year = input("Enter a year: ")
+while True:
+	try:
+		year = int(input("Enter a year: "))
+	except ValueError: 
+		print("Input is invalid. Please enter an integer input(year)")
+		continue
+	break
 if (year%4) != 0:
 	print(str(year) + " is not a leap year.")
 else:
